00dece5ffecf31e532af34e45d2caa0e231c5d13
Solon Galdino <solonfisicaufpb@gmail.com>
2023-04-07 02:19:13 -0300

    feat: inserido seleção de cat by name

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Olá mundo");
+#! /usr/bin/env node
+
+void (async () => {
+  try {
+    const args = process.argv;
+
+    delete args[0];
+    delete args[1];
+
+    if (args[2] === "rochele") {
+      console.log("Rouba comida em cima da mesa");
+      console.log("Rasga saco de lixo");
+    } else if (args[2] === "kiara") {
+      console.log("Influenciada por rochele e fofito");
+    } else if (args[2] === "fofito") {
+      console.log("Dorme muito");
+      console.log(
+        "Rouba comida em cima do fogão, balcão e qualquer comida facil."
+      );
+    } else if (args[2] === "lana") {
+      console.log("Observa tudo e não faz nada");
+      console.log("Caga todo dia no quartinho");
+    } else {
+      console.log("Digite o nome do gato por favor!");
+    }
+
+    console.log("[ END ]");
+  } catch (error) {
+    throw error;
+  }
+  process.exit();
+})();
